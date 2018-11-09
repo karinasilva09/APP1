e27d721c8e1cd4e2b9f43c41814bdaae0c57d443
Karina Henrique de Lima Silva <f573751@corp.caixa.gov.br>
2018-11-09 18:03:35 -0200

    Projeto

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,10 +12,35 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza esta frase'
+  public resposta: string
 
-  constructor() {}
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progresso: number = 0
+
+  constructor(){
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase)
+  }
 
   ngOnInit() {
   }
 
+  public atualizaResposta(reposta: Event): void{
+    this.resposta = (<HTMLInputElement>reposta.target).value
+    //console.log(this.resposta)
+  }
+
+  public verificarRespostacls(): void{
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      this.rodada++
+      this.rodadaFrase = this.frases[this.rodada]
+      this.progresso = this.progresso + 100/(this.frases.length)
+      console.log(this.progresso)
+    }else{
+      alert("Tradução incorreta")
+    }
+    
+  }
 }
